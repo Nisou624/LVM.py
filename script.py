5d2855eace2fc7d7daaf8c6f96fa2822c47ddfbb
Tilmatine Anis <anistilmatine6@gmail.com>
2023-12-01 21:33:16 +0100

    taking size from other LV/VG

--- script.py
+++ script.py
@@ -109,7 +109,6 @@ def calculate_and_sort_filesystems(file_systems):
     sorted_file_systems = sorted(file_systems, key=lambda fs: fs.get("writing_speed", 0), reverse=True)
     return sorted_file_systems
 
-#todo: add the condition that when we choose the active fs we should eliminate the fs we want to append
 def extendLV(lvName, Size="1G") -> None:
     c = subprocess.run(["sudo", "lvextend", "-L", f"+{Size}", f"/dev/mapper/{lvName}"],
                        capture_output=True, text=True)
@@ -121,16 +120,18 @@ def extendLV(lvName, Size="1G") -> None:
             potential_fs = [fs for fs in parsed_objects if fs["Available"] -(get_writing_speed(fs) * 1024 * 1024 * 3600) > fs["Size"] * 0.8]
             for fs in potential_fs:
                 if rstf <= 0 : break
-                asfe = (fs["Size"] * 0.7) - fs["Used"] #0.8 - 0.1  we take the threshold and we leave 10% for security 
-                if not is_filesystem_busy(fs):
+                asfe = (fs["Size"] * 0.7) - fs["Used"] #0.8 - 0.1  we take the threshold and we leave 10% for security  // asfe = available size for extension
+                if not is_filesystem_busy(fs) and fs["Filesystem"].split("/")[-1]  != lvName:
                     if rstf <= asfe:
                         unmount_filesystem(fs["Mount Point"])
-                        reduce_filesystem(lvName, fs["Filesystem"], fs["Mount Point"])
+                        reduce_filesystem(lvName, fs["Filesystem"], fs["Mount Point"], asfe)
                         remount_filesystem(lvName, fs["Mount Point"])
                         rstf -= asfe
                     else:
                         pass
             extendLV(lvName, Size) #check the logic of this shit
+            if c.returncode != 0:
+                logging.critical("There's no available space for extending")
     else:
         logging.info(c.stdout)
 
@@ -211,8 +212,28 @@ todo :
 - reduce the LV (size passed as param)
 - resize the FS
 """
-def reduce_filesystem(lv_name, filesystem_type, mount_point):
-    pass
+def reduce_filesystem(lv_name, filesystem_type, mount_point, new_size="1G"):
+    # Reduce the LV size using the 'lvreduce' command
+    subprocess.run(["sudo", "lvreduce", "-L", new_size, f"/dev/mapper/{lv_name}"], check=True)
+
+    # Run e2fsck before resizing
+    check_result = subprocess.run(["sudo", "e2fsck", "-f", "-y", f"/dev/mapper/{lv_name}"], capture_output=True, text=True)
+    if check_result.returncode != 0:
+        logging.error(f"Error running e2fsck: {check_result.stderr}")
+        return False
+
+    # Resize the filesystem based on the filesystem type
+    if "/xfs" in filesystem_type:
+        resize_result = subprocess.run(["sudo", "xfs_growfs", mount_point], capture_output=True, text=True)
+    else:
+        resize_result = subprocess.run(["sudo", "resize2fs", f"/dev/mapper/{lv_name}"], capture_output=True, text=True)
+
+    if resize_result.returncode != 0:
+        logging.error(f"Error resizing filesystem at {mount_point}: {resize_result.stderr}")
+    else:
+        logging.info(f"Resized filesystem at {mount_point}.")
+
+    return resize_result.returncode == 0
 
 
 def remount_filesystem(lv_name, mount_point):
